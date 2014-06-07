797764338c2da11f93e63ff3f8a7bac33d999fae
Ery <erylei@gmail.com>
2014-06-07 17:23:04 +0800

    suitup-kill

--- script/ps.sh
+++ script/ps.sh
@@ -7,14 +7,14 @@ function suitup-find-ps-id {
   ps aux | grep -v grep | grep $1 | awk '{print $2}'
 }
 
-function suitup-kill-all {
+function suitup-kill {
   for pid in `suitup-find-ps-id $1`
   do
-    suitup-kill $pid
+    suitup-kill-pid $pid
   done
 }
 
-function suitup-kill {
+function suitup-kill-pid {
   pid=$1
   echo "Kill $pid"
   sudo kill $pid
