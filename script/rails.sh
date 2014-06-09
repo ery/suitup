8710b6e607067ff9968a4c9a27bd4e06fb325844
Ery <erylei@gmail.com>
2014-06-09 10:31:39 +0800

    Rails Show log with key word

--- script/rails.sh
+++ script/rails.sh
@@ -4,16 +4,37 @@ function suitup-rails-assets-rebuild {
   bundle exec rake assets:precompile RAILS_ENV=development
 }
 
-function suitup-rails-show-log {
+function suitup-rails-show-log-all {
   tail -f log/development.log
 }
 
+# suitup-rails-show-log
+# suitup-rails-show-log keyword
+function suitup-rails-show-log {
+  key_word=$1
+  if [ -n "$key_word" ]; then
+    tail -f log/development.log | grep $key_word -C10
+  else
+    tail -f log/development.log
+  fi
+}
+
 function suitup-rails-show-log-production {
-  tail -f log/production.log
+  key_word=$1
+  if [ -n "$key_word" ]; then
+    tail -f log/production.log | grep $key_word -C10
+  else
+    tail -f log/production.log
+  fi
 }
 
 function suitup-rails-show-log-test {
-  tail -f log/test.log
+  key_word=$1
+  if [ -n "$key_word" ]; then
+    tail -f log/test.log | grep $key_word -C10
+  else
+    tail -f log/test.log
+  fi
 }
 
 function suitup-zeus-test {
