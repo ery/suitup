4e6f0564b1bd1420f7e6cd1bc272bb421e0c5ab7
Ery <erylei@gmail.com>
2014-06-20 18:29:14 +0800

    suitup path

--- script/suitup_bash.sh
+++ script/suitup_bash.sh
@@ -2,7 +2,7 @@ function suitup-current-dir {
   cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 }
 
-_suitup_path="$( suitup-current-dir )/.."
+_suitup_path="$(suitup-current-dir)/.."
 
 source "$_suitup_path/script/suitup.sh"
 suitup-load-script $_suitup_path
--- script/suitup_zsh.sh
+++ script/suitup_zsh.sh
@@ -1,16 +1,6 @@
 
-# relative
-_script_relative_path="`dirname \"$0\"`"
-
-# absolutized and normalized
-_script_absolutized_path="`( cd \"$_script_relative_path\" && pwd )`"
-if [ -z "$_script_absolutized_path" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
-fi
-
-_suitup_path="$_script_absolutized_path/.."
+_suitup_path="$(dirname $0)/.."
 
 source "$_suitup_path/script/suitup.sh"
+
 suitup-load-script $_suitup_path
