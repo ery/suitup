40ff23dd2ab94dc69875edc42e28b0c664a00e66
WangLei <wangl@udesk.cn>
2017-01-25 10:43:11 +0800

    mysql

--- script/mysql.sh
+++ script/mysql.sh
@@ -1,81 +1,88 @@
+VAR_SUITUP_MYSQL_USER='root'
+VAR_SUITUP_MYSQL_PASSWORD='root'
 
 function suitup-mysql-show-log {
   tail -f /var/log/mysql/mysql.log
 }
 
-function suitup-mysql-show-databases {
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e'SHOW DATABASES'
+function suitup-mysql-command {
+  command="${1}"
+
+  user=${VAR_SUITUP_MYSQL_USER}
+  password=${VAR_SUITUP_MYSQL_PASSWORD}
+
+  echo ${command}
+
+  mysql -u${user} -p${password} -e${command}
+}
+
+function suitup-mysql-command-db {
+  database=${1}
+  command="${2}"
+
+  user=${VAR_SUITUP_MYSQL_USER}
+  password=${VAR_SUITUP_MYSQL_PASSWORD}
+
+  echo ${command}
+
+  mysql -u${user} -p${password} ${database} -e${command}
+}
+
+function suitup-mysql-script {
+  database=${1}
+  script=${2}
+
+  user=${VAR_SUITUP_MYSQL_USER}
+  password=${VAR_SUITUP_MYSQL_PASSWORD}
+
+  mysql -u${user} -p${password} ${database} < ${script}
 }
 
 function suitup-mysql-show-tables {
-  database=$1
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"USE $database; SHOW TABLES"
+  database=${1}
+  suitup-mysql-command-db ${database} "SHOW TABLES"
 }
 
 function suitup-mysql-select-table {
-  database=$1
-  table=$2
+  database=${1}
+  table=${2}
   columns=$3
 
-  if [ -n "$columns" ]; then
-    columns=$columns
+  if [ -n "${columns}" ]; then
+    columns=${columns}
   else
     columns="*"
   fi
 
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"USE $database; SELECT $columns FROM $table LIMIT 200"
-}
-
-function suitup-mysql-drop-database {
-  database=$1
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"DROP DATABASE IF EXISTS $database"
-}
-
-function suitup-mysql-create-database {
-  database=$1
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"CREATE DATABASE IF NOT EXISTS $database DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
-}
-
-function suitup-mysql-command {
-  database=$1
-  command=$2
-  user='root'
-  password='root'
-  mysql -u$user -p$password $database -e"$command"
-}
-
-function suitup-mysql-script {
-  database=$1
-  script=$2
-  user='root'
-  password='root'
-  mysql -u$user -p$password $database < $script
+  suitup-mysql-command-db ${database} "SELECT ${columns} FROM ${table} LIMIT 200"
 }
 
 function suitup-mysql-desc-table {
-  database=$1
-  table=$2
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"USE $database; DESC $table"
+  database=${1}
+  table=${2}
+
+  suitup-mysql-command-db ${database} "DESC ${table}"
 }
 
 function suitup-mysql-show-index {
-  database=$1
-  table=$2
-  user='root'
-  password='root'
-  mysql -u$user -p$password -e"USE $database; SHOW INDEX FROM $table"
+  database=${1}
+  table=${2}
+
+  suitup-mysql-command-db ${database} "SHOW INDEX FROM ${table}"
+}
+
+function suitup-mysql-show-databases {
+  suitup-mysql-command 'SHOW DATABASES'
+}
+
+function suitup-mysql-drop-database {
+  database=${1}
+  suitup-mysql-command "DROP DATABASE IF EXISTS ${database}"
+}
+
+function suitup-mysql-create-database {
+  database=${1}
+  suitup-mysql-command "CREATE DATABASE IF NOT EXISTS ${database} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
 }
 
 
@@ -83,7 +90,7 @@ function suitup-mysql-show-index {
 # suitup-mysql-backup jason.com blog     red  redps    ~/backup/jason_blog
 function suitup-mysql-backup {
   server=$1
-  database=$2
+  database=${2}
   user=$3
   password=$4
   local_folder=$5
@@ -108,7 +115,7 @@ function suitup-mysql-backup {
   ssh $server "mkdir -p ${server_folder}"
 
   echo "Dumping MySQL Database ${database} ...."
-  ssh $server "mysqldump --opt --user=$user --password=$password $database > $server_file"
+  ssh $server "mysqldump --opt --user=$user --password=$password ${database} > $server_file"
   echo ""
 
   echo "Downloading backup file ...."
